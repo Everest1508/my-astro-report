1f09e85d5e4c7f93ed1e7db28b6bc34461317d29
Everest1508 <riteshmahale15@gmail.com>
2025-04-14 16:19:35 +0530

    Add import_export app to settings and update requirements; refactor admin classes to use ImportExportModelAdmin

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kundali',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- kundali/admin.py
+++ kundali/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from import_export.admin import ImportExportModelAdmin
 from .models import (
     Planet, Rashi, Nakshatra, Element, Awastha, House,
     UserKundali, PlanetPosition, PredictionResult,
     Language, Alias
 )
-
 
 # ----------------------------
 # Inline for Aliases
@@ -15,117 +15,108 @@ class AliasInline(GenericTabularInline):
     extra = 1
     autocomplete_fields = ['language']
 
-
 # ----------------------------
 # Language Admin
 # ----------------------------
 @admin.register(Language)
-class LanguageAdmin(admin.ModelAdmin):
+class LanguageAdmin(ImportExportModelAdmin):
     list_display = ['name', 'code']
     search_fields = ['name', 'code']
-
 
 # ----------------------------
 # Alias Admin
 # ----------------------------
 @admin.register(Alias)
-class AliasAdmin(admin.ModelAdmin):
+class AliasAdmin(ImportExportModelAdmin):
     list_display = ['name', 'language', 'content_type', 'object_id']
     search_fields = ['name']
     list_filter = ['language', 'content_type']
-
 
 # ----------------------------
 # Planet Admin
 # ----------------------------
 @admin.register(Planet)
-class PlanetAdmin(admin.ModelAdmin):
+class PlanetAdmin(ImportExportModelAdmin):
     list_display = ['name', 'is_visible', 'is_shadow', 'is_lagna']
     search_fields = ['name']
     list_filter = ['is_visible', 'is_shadow', 'is_malefic']
     inlines = [AliasInline]
 
-
 # ----------------------------
 # Rashi Admin
 # ----------------------------
 @admin.register(Rashi)
-class RashiAdmin(admin.ModelAdmin):
+class RashiAdmin(ImportExportModelAdmin):
     list_display = ['name', 'lord', 'element']
     search_fields = ['name', 'lord__name']
     autocomplete_fields = ['lord', 'element']
     list_filter = ['element']
     inlines = [AliasInline]
 
-
 # ----------------------------
 # Nakshatra Admin
 # ----------------------------
 @admin.register(Nakshatra)
-class NakshatraAdmin(admin.ModelAdmin):
+class NakshatraAdmin(ImportExportModelAdmin):
     list_display = ['name', 'lord']
     search_fields = ['name', 'lord__name']
     inlines = [AliasInline]
-
 
 # ----------------------------
 # Element Admin
 # ----------------------------
 @admin.register(Element)
-class ElementAdmin(admin.ModelAdmin):
-    list_display = ['name',]
+class ElementAdmin(ImportExportModelAdmin):
+    list_display = ['name']
     search_fields = ['name']
     inlines = [AliasInline]
-
 
 # ----------------------------
 # Awastha Admin
 # ----------------------------
 @admin.register(Awastha)
-class AwasthaAdmin(admin.ModelAdmin):
+class AwasthaAdmin(ImportExportModelAdmin):
     list_display = ['name']
     search_fields = ['name']
-
 
 # ----------------------------
 # House Admin
 # ----------------------------
 @admin.register(House)
-class HouseAdmin(admin.ModelAdmin):
+class HouseAdmin(ImportExportModelAdmin):
     list_display = ['number']
 
-
 # ----------------------------
-# UserKundali Admin
+# PlanetPosition Inline for UserKundali
 # ----------------------------
 class PlanetPositionInline(admin.TabularInline):
     model = PlanetPosition
     extra = 1
 
-
+# ----------------------------
+# UserKundali Admin
+# ----------------------------
 @admin.register(UserKundali)
-class UserKundaliAdmin(admin.ModelAdmin):
+class UserKundaliAdmin(ImportExportModelAdmin):
     list_display = ['user', 'chart_type', 'ascendant', 'created_at']
     list_filter = ['chart_type', 'created_at']
     search_fields = ['user__username']
     inlines = [PlanetPositionInline]
 
-
 # ----------------------------
 # PlanetPosition Admin
 # ----------------------------
 @admin.register(PlanetPosition)
-class PlanetPositionAdmin(admin.ModelAdmin):
+class PlanetPositionAdmin(ImportExportModelAdmin):
     list_display = ['kundali', 'planet', 'sign', 'house', 'is_retro']
     list_filter = ['planet', 'sign', 'house', 'is_retro']
     search_fields = ['kundali__user__username']
-
 
 # ----------------------------
 # PredictionResult Admin
 # ----------------------------
 @admin.register(PredictionResult)
-class PredictionResultAdmin(admin.ModelAdmin):
+class PredictionResultAdmin(ImportExportModelAdmin):
     list_display = ['kundali', 'category', 'created_at']
     list_filter = ['category', 'created_at']
     search_fields = ['kundali__user__username', 'category']
