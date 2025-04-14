09a4cfade61e4c927e407d0bbc7f7d8cf0c5b848
Everest1508 <riteshmahale15@gmail.com>
2025-04-14 16:56:43 +0530

    Update ALLOWED_HOSTS in settings to include production domain

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g*i)d1qstlh=a8r240laej9r#gb%f!o3a4at6y-@t++!*qh57o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["myastro.pythonanywhere.com"]
 
 
 # Application definition
